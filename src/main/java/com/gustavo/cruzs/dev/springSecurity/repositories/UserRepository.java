377ec451b7fdbfc6bcfade0cf70314f68a6eea6e
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-05-05 15:26:12 -0300

    feat(user): cria a repository

---
+++ src/main/java/com/gustavo/cruzs/dev/springSecurity/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gustavo.cruzs.dev.springSecurity.repositories;
+
+import com.gustavo.cruzs.dev.springSecurity.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
