d8966a16eef98626c78a9c61595febf7d4d7b8aa
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-05-05 09:15:47 -0300

    project initial

---
+++ src/test/java/com/gustavo/cruzs/dev/springSecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.cruzs.dev.springSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
