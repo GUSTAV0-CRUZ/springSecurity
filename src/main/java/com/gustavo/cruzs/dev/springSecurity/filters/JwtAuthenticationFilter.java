0d90db0b6c380d1f741a065d2eb8db5bd0e45fdf
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-05-05 23:02:20 -0300

    feat(filter): cria filtro onde usa o jwt para capturar o usuario e a sua regra

---
+++ src/main/java/com/gustavo/cruzs/dev/springSecurity/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.gustavo.cruzs.dev.springSecurity.filters;
+
+import com.gustavo.cruzs.dev.springSecurity.entities.User;
+import com.gustavo.cruzs.dev.springSecurity.repositories.UserRepository;
+import com.gustavo.cruzs.dev.springSecurity.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  private final JwtService jwtService;
+  private final UserRepository userRepository;
+
+  public JwtAuthenticationFilter(JwtService jwtService, UserRepository userRepository) {
+    this.jwtService = jwtService;
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+  ) throws ServletException, IOException {
+    String token = request.getHeader("Authorization");
+
+    if(token != null && token.startsWith("Bearer ")) {
+      String tokenExtracted = token.replace("Bearer ", "");
+      String email = jwtService.validateToken(tokenExtracted);
+
+      userRepository.findByEmail(email).ifPresent(user -> {
+          var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+          SecurityContextHolder.getContext().setAuthentication(authentication);
+         }
+      );
+    }
+
+    filterChain.doFilter(request, response);
+  }
+}
